0148eca5e9a8c7c761486919b353b321221b5135
Jose Francisco Robles Sapien <jrobles@ibm.com>
2021-07-13 10:58:57 -0500

    First Observable

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
+import { Observable } from 'rxjs';
 
+//Observable; es recomendable especificar el tipo de valor que fluye del observable
+const obs$ = new Observable<string>( subscriber => {
+    subscriber.next('Hola');
+    subscriber.next('Mundo');
 
-console.log('Hola Mundo!');
+    subscriber.next('Hola');
+    subscriber.next('Mundo');
 
+    //Después del complete, no habrá más salidas de información aunque allá emisiones del observable.
+    subscriber.complete();
 
+    subscriber.next('Hola');
+    subscriber.next('Mundo');
+});
+
+obs$.subscribe( console.log )
 
 
 
